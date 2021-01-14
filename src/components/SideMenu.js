8120212c40f73c8fb7f174e04a3a238478a52822
ayvicky1 <ayvicky1@gmail.com>
2021-01-14 10:39:22 +0500

    add styles for side-menu which is HOC using withStyles()

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -4,7 +4,19 @@ import { makeStyles, withStyles } from '@material-ui/core';
 
 // withStyles & makeStyles
 
-const useStyles = makeStyles({
+// const useStyles = makeStyles({
+//     sideMenu: {        
+//         display: 'flex',
+//         displayDirection: 'column',
+//         position: 'absolute',
+//         left: 0,
+//         width: '320px',
+//         height: '100%',
+//         backgroundColor: '#253053',
+//     }
+// });
+
+const styles = {
     sideMenu: {        
         display: 'flex',
         displayDirection: 'column',
@@ -14,13 +26,16 @@ const useStyles = makeStyles({
         height: '100%',
         backgroundColor: '#253053',
     }
-});
+};
 
-export default function SideMenu() {
-    const classes = useStyles();
+const SideMenu = (props) => {
+    const {classes} = props;
+    console.log(classes);
     return (
         <div className={classes.sideMenu}>
             
         </div>
     )
 }
+
+export default withStyles(styles)(SideMenu);
