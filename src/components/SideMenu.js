1eeaff8b6d6cbb61906ecbcf780541a274cd332d
ayvicky1 <ayvicky1@gmail.com>
2021-01-14 10:30:50 +0500

    make use of css-in-js & styled SideMenu

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
+import { makeStyles, withStyles } from '@material-ui/core';
+
+// withStyles & makeStyles
+
+const useStyles = makeStyles({
+    sideMenu: {        
+        display: 'flex',
+        displayDirection: 'column',
+        position: 'absolute',
+        left: 0,
+        width: '320px',
+        height: '100%',
+        backgroundColor: '#253053',
+    }
+});
+
 export default function SideMenu() {
+    const classes = useStyles();
     return (
-        <div className='side-menu'>
+        <div className={classes.sideMenu}>
             
         </div>
     )
