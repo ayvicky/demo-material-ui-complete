4fd35f3df206f8117ff743dba09384c5b2e7bbc9
ayvicky1 <ayvicky1@gmail.com>
2021-01-14 10:41:56 +0500

    added app-main div & styling

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 
 import SideMenu from '../components/SideMenu';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  appMain: {
+    paddingLeft: '320px',
+    width: '100%'
+  }
+})
 
 function App() {
+  const classes = useStyles();
+
   return (
     <>
       <SideMenu />
-      <div>Here we go!</div>
+      <div className={classes.appMain}>
+        Here we go!
+      </div>
     </>
     );
 }
