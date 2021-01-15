2aa8c7d844b24168e360b75e466163b71abb23c8
ayvicky1 <ayvicky1@gmail.com>
2021-01-15 18:46:30 +0500

    added styles to popup

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,11 +1,21 @@
 import React from 'react'
 
-import { Dialog, DialogContent, DialogTitle } from '@material-ui/core';
+import { Dialog, DialogContent, DialogTitle, makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    dialogWrapper: {
+        padding: theme.spacing(2),
+        position: 'absolute',
+        top: theme.spacing(5)
+    }
+}))
 
 export default function Popup(props) {
-    const { title, children, openPopup, setOpenPopup} = props;
+    const { title, children, openPopup, setOpenPopup} = props
+    const classes = useStyles()
+
     return (
-        <Dialog open={openPopup}>
+        <Dialog open={openPopup} maxWidth='md' classes={{ paper: classes.dialogWrapper}}>
             <DialogTitle>
                 {title}
             </DialogTitle>
