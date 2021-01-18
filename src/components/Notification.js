2ad113a8d0fedb79b228732fc92383c77e430137
ayvicky1 <ayvicky1@gmail.com>
2021-01-18 10:19:16 +0500

    setup basic notification -reuseable component

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,17 +1,37 @@
 import React from 'react'
 
-import { Snackbar } from '@material-ui/core'
+import { makeStyles, Snackbar } from '@material-ui/core'
 import { Alert } from '@material-ui/lab'
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        top: theme.spacing(9)
+    }
+}))
 
 export default function Notification(props) {
     const { notify, setNotify } = props
+    const classes = useStyles()
+
+    const handleClose = (event, reason) => {
+        if(reson === 'clickaway') return
+        setNotify({
+            ...notify,
+            isOpen: false
+        })
+    }
+
     return (
         <Snackbar
+            className={classes.root}
             open={notify.isOpen}
             autoHideDuration={3000}
             anchorOrigin={{vertical:'top', horizontal: 'right'}}
+            onClose={handleClose}
             >
-                <Alert severity={notify.type}>
+                <Alert
+                    onClose={handleClose}
+                    severity={notify.type}>
                     {notify.message}
                 </Alert>
         </Snackbar>
