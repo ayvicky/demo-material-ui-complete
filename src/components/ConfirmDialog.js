3d1dbb1f84fb75fff629969326f95a35b34fe982
ayvicky1 <ayvicky1@gmail.com>
2021-01-18 11:25:15 +0500

    added styling to Confirm Dialog

--- src/components/ConfirmDialog.js
+++ src/components/ConfirmDialog.js
@@ -1,17 +1,32 @@
 import React from 'react'
-import { Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@material-ui/core'
+import { Dialog, DialogActions, DialogContent, DialogTitle, makeStyles, Typography } from '@material-ui/core'
 
 import { Controls } from './controls/Controls'
 
+const useStyles = makeStyles(theme => ({
+    dialog: {
+        padding: theme.spacing(2),
+        position: 'absolute',
+        top: theme.spacing(5)
+    },
+    dialogContent: {
+        textAlign: 'center'
+    },
+    dialogActions: {
+        justifyContent: 'center'
+    }
+}))
+
 export default function ConfirmDialog(props) {
-    const { confirmDialog, setConfirmDialog } = props;
+    const { confirmDialog, setConfirmDialog } = props
+    const classes = useStyles()
 
     return (
-        <Dialog open={confirmDialog.isOpen}>
+        <Dialog open={confirmDialog.isOpen} classes={{paper: classes.dialog}}>
             <DialogTitle>
 
             </DialogTitle>
-            <DialogContent>
+            <DialogContent className={classes.dialogContent}>
                 <Typography variant='h6'>
                     {confirmDialog.title}
                 </Typography>
@@ -19,7 +34,7 @@ export default function ConfirmDialog(props) {
                     {confirmDialog.subTitle}
                 </Typography>
             </DialogContent>
-            <DialogActions>
+            <DialogActions className={classes.dialogActions}>
                 <Controls.Button
                     text='NO'
                     color='default'
